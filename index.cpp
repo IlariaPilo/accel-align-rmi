3cf87b3e91b53018a5ae13be5a27968fae8c28ed
yanlolo <yanlolo2012@gmail.com>
2022-04-19 17:13:51 +0200

    template for index

--- index.cpp
+++ index.cpp
@@ -6,11 +6,12 @@ const unsigned mod = (1UL << 29) - 1;
 unsigned step = 1;
 unsigned kmer;
 
+template <typename T>
 struct Data {
   uint32_t key;
-  uint64_t pos;
+  T pos;
   Data() : key(-1), pos(-1) {}
-  Data(uint32_t k, uint64_t p) : key(k), pos(p) {}
+  Data(uint32_t k, T p) : key(k), pos(p) {}
 
   bool operator()(const Data &X, const Data &Y) const {
     return X.key == Y.key ? X.pos < Y.pos : X.key < Y.key;
@@ -18,16 +19,18 @@ struct Data {
 
 };
 
+template <typename T>
 class Index {
  private:
   string ref;
  public:
   bool load_ref(const char *F);
   bool make_index(const char *F);
-  void cal_key(size_t i, vector<Data> &data);
+  void cal_key(size_t i, vector<Data<T>> &data);
 };
 
-bool Index::load_ref(const char *F) {
+template <class T>
+bool Index<T>::load_ref(const char *F) {
   char code[256], buf[65536];
   for (size_t i = 0; i < 256; i++)
     code[i] = 4;
@@ -57,7 +60,8 @@ bool Index::load_ref(const char *F) {
   return true;
 }
 
-void Index::cal_key(size_t i, vector<Data> &data) {
+template <class T>
+void Index<T>::cal_key(size_t i, vector<Data<T>> &data) {
   uint64_t h = 0;
   bool hasn = false;
   for (unsigned j = 0; j < kmer; j++) {
@@ -72,12 +76,13 @@ void Index::cal_key(size_t i, vector<Data> &data) {
   }
 }
 
+template <class T>
 class Tbb_cal_key {
-  vector<Data> &data;
-  Index *index_obj;
+  vector<Data<T>> &data;
+  Index<T> *index_obj;
 
  public:
-  Tbb_cal_key(vector<Data> &_data, Index *_index_obj) :
+  Tbb_cal_key(vector<Data<T>> &_data, Index<T> *_index_obj) :
       data(_data), index_obj(_index_obj) {}
 
   void operator()(const tbb::blocked_range<size_t> &r) const {
@@ -87,7 +92,8 @@ class Tbb_cal_key {
   }
 };
 
-bool Index::make_index(const char *F) {
+template <class T>
+bool Index<T>::make_index(const char *F) {
   size_t limit = ref.size() - kmer + 1;
   size_t vsz;
   if (step == 1)
@@ -95,10 +101,10 @@ bool Index::make_index(const char *F) {
   else
     vsz = ref.size() / step + 1;
 
-  vector<Data> data(vsz, Data());
+  vector<Data<T>> data(vsz, Data<T>());
   cerr << "hashing :limit = " << limit << ", vsz = " << vsz << endl;
 
-  tbb::parallel_for(tbb::blocked_range<size_t>(0, limit), Tbb_cal_key(data, this));
+  tbb::parallel_for(tbb::blocked_range<size_t>(0, limit), Tbb_cal_key<T>(data, this));
   cerr << "hash\t" << data.size() << endl;
 
   //XXX: Parallel sort uses lots of memory. Need to fix this. In general, we
@@ -106,10 +112,10 @@ bool Index::make_index(const char *F) {
   try {
     cerr << "Attempting parallel sorting\n";
     tbb::task_scheduler_init init(tbb::task_scheduler_init::automatic);
-    tbb::parallel_sort(data.begin(), data.end(), Data());
+    tbb::parallel_sort(data.begin(), data.end(), Data<T>());
   } catch (std::bad_alloc e) {
     cerr << "Fall back to serial sorting (low mem)\n";
-    sort(data.begin(), data.end(), Data());
+    sort(data.begin(), data.end(), Data<T>());
   }
 
   cerr << "writing\n";
@@ -118,27 +124,27 @@ bool Index::make_index(const char *F) {
   ofstream fo(fn.c_str(), ios::binary);
 
   // determine the number of valid entries based on first junk entry
-  auto joff = std::lower_bound(data.begin(), data.end(), Data(-1, -1), Data());
+  auto joff = std::lower_bound(data.begin(), data.end(), Data<T>(-1, -1), Data<T>());
   size_t eof = joff - data.begin();
   cerr << "Found " << eof << " valid entries out of " <<
        data.size() << " total\n";
-  fo.write((char *) &eof, sizeof(uint64_t));
+  fo.write((char *) &eof, sizeof(T));
 
   // write out keys
   for (size_t i = eof; i < data.size(); i++)
     assert(data[i].key == (uint32_t) -1);
   try {
     cerr << "Fast writing posv (" << eof << ")\n";
-    uint64_t *buf = new uint64_t[eof];
+    T *buf = new T[eof];
     for (size_t i = 0; i < eof; i++) {
       buf[i] = data[i].pos;
     }
-    fo.write((char *) buf, eof * sizeof(uint64_t));
+    fo.write((char *) buf, eof * sizeof(T));
     delete[] buf;
   } catch (std::bad_alloc e) {
     cerr << "Fall back to slow writing posv due to low mem.\n";
     for (size_t i = 0; i < eof; i++) {
-      fo.write((char *) &data[i].pos, sizeof(uint64_t));
+      fo.write((char *) &data[i].pos, sizeof(T));
     }
   }
 
@@ -146,7 +152,7 @@ bool Index::make_index(const char *F) {
   try {
     cerr << "Fast writing keyv\n";
     size_t buf_idx = 0;
-    uint64_t *buf = new uint64_t[mod + 1];
+    T *buf = new T[mod + 1];
     for (size_t i = 0; i < eof;) {
       assert (data[i].key != (uint32_t) -1);
       size_t h = data[i].key, n;
@@ -165,7 +171,7 @@ bool Index::make_index(const char *F) {
       ++buf_idx;
     }
     assert(buf_idx == (mod + 1));
-    fo.write((char *) buf, buf_idx * sizeof(uint64_t));
+    fo.write((char *) buf, buf_idx * sizeof(T));
     delete[] buf;
   } catch (std::bad_alloc e) {
     cerr << "Fall back to slow writing keyv (low mem)\n";
@@ -174,7 +180,7 @@ bool Index::make_index(const char *F) {
       size_t h = data[i].key, n;
       offset = i;
       for (size_t j = last_key; j <= h; j++) {
-        fo.write((char *) &offset, sizeof(uint64_t));
+        fo.write((char *) &offset, sizeof(T));
       }
       last_key = h + 1;
       for (n = i + 1; n < eof && data[n].key == h; n++);
@@ -182,7 +188,7 @@ bool Index::make_index(const char *F) {
     }
     offset = eof;
     for (size_t j = last_key; j <= mod; j++) {
-      fo.write((char *) &offset, sizeof(uint64_t));
+      fo.write((char *) &offset, sizeof(T));
     }
   }
 
@@ -197,14 +203,18 @@ int main(int ac, char **av) {
     cerr << "options:\n";
     cerr << "\t-l INT length of seed [32]\n";
     cerr << "\t-s INT step of seed [1]\n";
+    cerr << "\t-L large genome\n";
     return 0;
   }
   unsigned kmer_temp = 0;
+  bool large_genome = false;
   for (int it = 1; it < ac; it++) {
     if (strcmp(av[it], "-l") == 0)
       kmer_temp = atoi(av[it + 1]);
-    if (strcmp(av[it], "-s") == 0)
+    else if (strcmp(av[it], "-s") == 0)
       step = atoi(av[it + 1]);
+    else if (strcmp(av[it], "-L") == 0)
+      large_genome = true;
   }
   kmer = 32;
   if (kmer_temp != 0)
@@ -212,10 +222,19 @@ int main(int ac, char **av) {
 
   cerr << "Using kmer length " << kmer << " and step size " << step << endl;
 
-  Index i;
-  if (!i.load_ref(av[ac - 1]))
-    return 0;
-  if (!i.make_index(av[ac - 1]))
-    return 0;
+  if (large_genome){
+    Index<uint64_t> i;
+    if (!i.load_ref(av[ac - 1]))
+      return 0;
+    if (!i.make_index(av[ac - 1]))
+      return 0;
+  } else{
+    Index<uint32_t> i;
+    if (!i.load_ref(av[ac - 1]))
+      return 0;
+    if (!i.make_index(av[ac - 1]))
+      return 0;
+  }
+
   return 0;
 }
