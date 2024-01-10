b839baa1078950c07067f9800477ee942d90b65b
Ilaria Pilo <ilaria.pilo@studenti.polito.it>
2024-01-10 16:37:33 +0100

    Add input check

--- utilities/model_select.sh
+++ utilities/model_select.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-read -ep $'\033[1;33m [index.sh] \033[0mwhich index would you like to train? [1-10] ' choice
+while true; do
+    read -ep $'\033[1;33m [index.sh] \033[0mwhich index would you like to train? [1-10] ' choice
+
+    # Check if the input is a number
+    if [[ "$choice" =~ ^[1-9]$|^10$ ]]; then
+        break  # Break out of the loop if it's a valid number in the range [1-10]
+    else
+        echo "            Please enter a number between 1 and 10."
+    fi
+done
 
 choice=$((choice + 1))
 
