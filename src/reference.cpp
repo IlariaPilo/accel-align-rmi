1022642aa87d399fd3ef0a6a322da61c5a7ec5ca
Ilaria Pilo <ilaria.pilo@studenti.polito.it>
2024-01-11 15:57:42 +0100

    raccoon brain

--- src/reference.cpp
+++ src/reference.cpp
@@ -383,7 +383,7 @@ Reference::Reference(const char *F, int bit_len, bool _enable_minimizer, char _m
     string F_index = F_prefix  + "_index" + to_string(bit_len/2);
     string F_library = F_index + "/" + get_last_directory(F_index);
     rmi.init(F_library.c_str());
-    thread t([this, F]() {load_index(F);});
+    thread t([this, F_index]() {load_index(F_index.c_str());});
     //thread t(&Reference::load_index, this, F_index.c_str()); // load index in parallel
 
     load_reference(F);
